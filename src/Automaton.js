7d6eac5d0f89c5c2f661e674c89b75bb0cf465df
Charbel Naba <charbelnaba@gmail.com>
2021-08-25 20:24:37 +0300

    Implemented the readCommands

--- src/Automaton.js
+++ src/Automaton.js
@@ -2,12 +2,40 @@ export default class Automaton {
   constructor(props) {
     // Implement this
     // Do any setup you think you need for the Automaton to work
+    this.props=props;
   }
 
 
   readCommands(commands) {
     // Implement this
     // Read in an commands ([String])
-    // Return true if we end in our accept state, false otherwise.  
+    // Return true if we end in our accept state, false otherwise. 
+    //Les states q1, q2, q3 sont respectivement 1, 2 et 3 dans le code 
+    var state=1; //q1
+    for (let i=0; i<commands.length; i++) {
+      if (state==1 && commands[i]==1){
+        state=2;
+      }else if (state==1 && commands[i]==0){
+        state=1;
+      }else if(state==2 && commands[i]==0){
+        state=3;
+      }else if(state==2 && commands[i]==1){
+        state=2;
+      }else if(state==3 && (commands[i]==0||commands[i]==1)){
+        state=2;
+      }
+    }
+    if (state==2){
+      return true;
+    }
+    else{
+      return false;
+    } 
   }
+
+  
 }
+
+var a = new Automaton();
+// Do anything you need to set up this automaton's states.
+var isAccepted = a.readCommands(["1", "0", "0", "1", "0"]);
